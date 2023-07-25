8b52c8474e0fc057a44d692e479002bacaa8bb8d
darshitagarwal <darshitagarwal02@gmail.com>
2023-07-25 21:03:44 +0530

    xchange

--- welcome.py
+++ welcome.py
@@ -1,23 +1,22 @@
-# print("Hello World")
-a=32
-b=32.32
-c=31+41j
-print(a,b,c," ",type(a)," ",type(b)," ",type(c))
+# # print("Hello World")
+# a=32
+# b=32.32
+# c=31+41j
+# print(a,b,c," ",type(a)," ",type(b)," ",type(c))
+# class loops:
+#     def forloop():
+#         x=int(input("Enter a value-> "))
+#         y=int(input("Enter the second value-> "))
+#         for i in range(x,y,2):
+#             print(i,end=" ")
+# loops.forloop()
+# array_1=[1,2,3,4,5,6,"test1"]
+# print(array_1,type(array_1))
+# print(array_1[-1])
+# print(array_1,type(array_1))
+#Design a Calculator
 
 
-class loops:
-    def forloop():
-        x=int(input("Enter a value-> "))
-        y=int(input("Enter the second value-> "))
-        for i in range(x,y,2):
-            print(i,end=" ")
-loops.forloop()
-
-
-array_1=[1,2,3,4,5,6,"test1"]
-print(array_1,type(array_1))
-print(array_1[-1])
-print(array_1,type(array_1))
 
 
  
